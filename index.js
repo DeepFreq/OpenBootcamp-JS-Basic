f826f8aa4a30d91db667c39629d1e4906aa47f1b
Alan L. Acosta <alanegocios77@gmail.com>
2022-08-19 02:10:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// This is the welcome message
+console.log("This is the entry door to the project");
+
+// This is the intermediate part of our project
+// In this part we will realize the necessary checks
+// Finally got to the last line of greetings
+
+/*
+This is the intermediate part of our project
+In this part we will realize the necessary checks
+Finally got to the last line of greetings
+*/
+
+// This being the message go send to goodbye
+console.log("Goodbye");
